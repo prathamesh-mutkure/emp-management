99d887268322050ac110095996809ea82047dad2
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-06-16 01:47:05 +0530

    Models Generated

---
+++ types/models.d.ts
@@ -0,0 +1,41 @@
+interface Department {
+  creation_date: string;
+  department_head: string;
+  department_id: number;
+  department_name: string;
+  description: string;
+  last_update_date: string;
+}
+
+interface Employee {
+  firstname: string;
+  lastname: string;
+  username: string;
+  emailid: string;
+  password: string;
+  mobileno: number;
+  department: Department;
+  roles: string[];
+  type: string;
+  city: string;
+  street: string;
+  pincode: string;
+  question: string;
+  answer: string;
+  createdDate: number;
+  lastUpdateDdate: string;
+}
+
+interface EmployeeDepartment {
+  username: string;
+  departments: Department[];
+}
+
+interface Role {
+  id: number;
+  name: string;
+}
+
+interface View {
+  contentType: string;
+}
